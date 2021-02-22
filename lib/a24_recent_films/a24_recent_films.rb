62c26a9a94c40d7706b99a552e9f2be1c380d087
Melissa Guachun <1212malka@gmail.com>
2021-02-22 15:32:10 -0500

    transferring data btw objects

--- lib/a24_recent_films/a24_recent_films.rb
+++ lib/a24_recent_films/a24_recent_films.rb
@@ -4,15 +4,39 @@
 
 class Films
   @@all = []
-  attr_accessor :title, :release, :info
-  #title, release, info
-  def initialize(movie, date, starring)
-    @movie = title
-    @date = release
-    @starring = info
+  attr_accessor :title, :release, :image
+  
+  def initialize()
+    @@all << self
   end
 
-  
-  @@all << self
+  def self.all_films
+    @@all_films 
+  end
+
+  def self.list_films
+    @@all_films.each.with_index(1) do {|obj, index| puts "#{index}. #{create_a24_obj.title}"}
+  end
+
+  def self.find_by_title(selected_title)
+    self.all_films.find {|obj|} create_a24_obj.title == selected_title
+  end
+
+  def find_and_show_film_obj(integer)
+    index = integer - 1
+    obj = Films.list_movie_titles[index - 1]
+    puts "Title:" = moviecard.css("figcaption h3").text
+    puts "Date Release:" = moviecard.css("figcaption h6")
+    puts "Image:" = moviecard.css("img")[0]['src']
+  end
+
+  #ADD LOOP
+
+
+end
+
+
+
+
 
 end
