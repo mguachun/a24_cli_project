1e75d654a3d38a1f155b9e888be97ae64fcedb53
Melissa Guachun <1212malka@gmail.com>
2021-02-22 15:31:30 -0500

    change constructof environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 
 #/Users/melissaguachun/upcoming_films/lib/upcoming_films/upcoming_films.rb
-require_relative "../lib/a24_recent_films/cli.rb"
 require_relative "../lib/a24_recent_films/a24_recent_films.rb"
 require_relative "../lib/a24_recent_films/scraper.rb"
+require_relative "../lib/a24_recent_films/cli.rb"
 #require_relative "../lib/version.rb"
