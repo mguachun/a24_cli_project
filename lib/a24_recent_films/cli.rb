645b3399cee028bb899c63f9cfb4977a3719c99a
Melissa Guachun <1212malka@gmail.com>
2021-02-22 15:32:22 -0500

    updated cli

--- lib/a24_recent_films/cli.rb
+++ lib/a24_recent_films/cli.rb
@@ -1,22 +1,32 @@
 class CLI
 
-    def begin
-        list_films
-        menu
+    def self.begin
+        #puts scraper list
+        howdy_user 
     end
 
-    def list_films
-        puts "Please enter the name of the movie you want to know more about."
+    def self.howdy_user 
+        puts "Welcome! Herer are the recent movies from A24 Studios!"
+        #put class method of list
+
+        num = self.ask_for_input
+
+        Films.find_and_show_movie_obj(num)
+    end
+
+    def self.ask_for_input
+        puts '\n'
+        puts "Please select a number from the list"
+        #gets.chomp.to_i
     end
 
     def sub_options
         puts "Press (e) to Exit"
         puts "Press (r) to return to Main Menu"
-    
     end
 
    def goodbye
-    puts "Thanks for stopping by!"
+        puts "Thanks for stopping by!"
    end
 
 end
