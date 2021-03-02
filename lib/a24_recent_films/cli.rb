7ecb5b1c7bd3b3ae1fea0e554668d840442c00cc
Melissa Guachun <1212malka@gmail.com>
2021-03-01 23:24:35 -0500

    cleaning up structure

--- lib/a24_recent_films/cli.rb
+++ lib/a24_recent_films/cli.rb
@@ -3,7 +3,7 @@ class CLI
     def begin
         Scraper.get_page 
         howdy_user 
-        ask_for_input
+        
 
     end
 
@@ -18,7 +18,9 @@ class CLI
 
         num = self.ask_for_input
 
-        Film.find_and_show_movie_obj(num)
+        puts ' '
+
+        Film.print_movie_info(num)
 
     end
 
@@ -41,17 +43,14 @@ class CLI
     #user chooses number and give back image and date released
     def print_movie_info
 
-        puts "Date Released: #{film_obj.release}"
-        puts "Movie Image: #{film_obj.image}"
         puts "Title: #{film_obj.title}"
-
+        puts ' '
+        puts "Image: #{film_obj.image}"
+        puts ' '
+        puts "Date Released: #{film_obj.release}"
+        
         sub_options
     end
-
-    def exit_film
-        puts "Thanks for stopping by!"
-   end
-
 
     def sub_options
         puts "Press (e) to Exit"
@@ -62,7 +61,6 @@ class CLI
             puts "Back to Main Menu"
             puts " "
             self.begin
-    
         elsif answer == "e"
             exit_film
             puts "Thanks for stopping by!"
@@ -73,31 +71,19 @@ class CLI
         end      
     end
 
-  # def list_films(index_of_titles)
-        #     puts " "
-        #     puts "#{Film.all[index_of_titles].title}"
-        #     puts " "
-        #     puts "#{Film.all[index_of_titles].release}"
-        #     puts " "
-        #     puts "#{Film.all[index_of_titles].image}"
-        #    end
-        
+    def exit_film
+        puts "Thanks for stopping by!"
+   end
 
-
-        # #this method will input a proper input between 1-10
-        # if input.to_i > 0 && input.to_i <= 10
-        #     index_of_titles = input.to_i - 1
-        # else
-        #     puts "Please try again!"
-        # end 
-        # #has to connect index with object
-        #     object = Film.all[5] 
-        #     moviecard.list_movie_titles[index_of_titles - 1] 
-        # end
-        # #connect movie title with date and image attr
-        # # Film.find_and_show_film_obj
-        # # puts ' '
-        # # #puts
+#   def list_films(index_of_titles)
+#             puts " "
+#             puts "#{Film.all[index_of_titles].title}"
+#             puts " "
+#             puts "#{Film.all[index_of_titles].release}"
+#             puts " "
+#             puts "#{Film.all[index_of_titles].image}"
+#            end
+    
 end
 
 
