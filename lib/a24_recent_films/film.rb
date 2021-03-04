cc7cb051860807b89ac83af2134847cc9e18b840
mguachun <53132301+mguachun@users.noreply.github.com>
2021-03-03 21:08:32 -0500

    Update film.rb

--- lib/a24_recent_films/film.rb
+++ lib/a24_recent_films/film.rb
@@ -22,15 +22,21 @@ class Film
   def self.find_by_title(selected_title)
     self.all.find {|obj| obj.title == selected_title}
   end
-#change name of method 
-  def self.find_and_show_film_obj(integer)
-    index = integer - 1
-    obj = Scraper.get_page[index]
-     puts "Title:#{obj.children[1].values[1]}"
-     binding.pry
 
-     puts "Release Date: #{film_obj.release}"
-    # puts "Image: #{film_obj.image}"
+  def self.print_movie_info(integer)
+    index = (integer - 1)
+    obj = @@all[index]
+    #^ accesses instances to get data from 
+    #scraper data instead of Scraper.get_page[index]
+    #allows access to Film instances stored in @@all
+    
+    puts "Title: #{obj.title}"
+    puts ' '
+    puts "Image: #{obj.image}"
+    puts ''
+    puts "Release Date: #{obj.release.text}"
+    puts ' '
+   
   end
 
 
