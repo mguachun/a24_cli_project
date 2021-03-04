c9db948f30979fe8db665f19644b0b4a9b6e3be2
mguachun <53132301+mguachun@users.noreply.github.com>
2021-03-03 21:06:17 -0500

    Update cli.rb

--- lib/a24_recent_films/cli.rb
+++ lib/a24_recent_films/cli.rb
@@ -3,8 +3,6 @@ class CLI
     def begin
         Scraper.get_page 
         howdy_user 
-        ask_for_input
-
     end
 
     def howdy_user 
@@ -14,91 +12,67 @@ class CLI
         puts "****************************************************"
         puts ' '
     
-        Film.list_film
+        Film.list_film 
 
-        num = self.ask_for_input
+        # num = self.ask_for_input
+        self.ask_for_input
 
-        Film.find_and_show_movie_obj(num)
+        puts ' '
+
+        # Film.print_movie_info(num)
+
+        # sub_options
 
     end
-
-    #while loop
-    # i = 0
-    # while i < 11 do
-    #     puts i 
-    #     i += 1
-    #     break if i == 12
-
-    # end
 
 
     def ask_for_input
         puts ' '
         puts "Please select a number from the list:"
         input = gets.chomp.to_i
-    end
-    
-    #user chooses number and give back image and date released
-    def print_movie_info
+     
 
-        puts "Date Released: #{film_obj.release}"
-        puts "Movie Image: #{film_obj.image}"
-        puts "Title: #{film_obj.title}"
+            if input >= 1 && input <= 9
+               Film.print_movie_info(input)
+               sub_options
+            else 
+                puts "Error, please try again!"
+                ask_for_input
+                
 
-        sub_options
+               
+            end 
     end
+    #^user chooses number and give back image and date released
 
     def exit_film
-        puts "Thanks for stopping by!"
+        puts "Come back soon to see the most recent films from A24 Studios!"    
    end
 
-
     def sub_options
-        puts "Press (e) to Exit"
-        puts "Press (r) to return to Main Menu"
+        puts "Would you like to see the list again? [y/n]"
         input = gets.strip.downcase
 
-        if answer == "r"
-            puts "Back to Main Menu"
+        if input == "y"
+            puts "Back to the recent A24 films list!"
             puts " "
-            self.begin
-    
-        elsif answer == "e"
+            howdy_user
+
+        elsif input == "n" || input == "exit"
             exit_film
+            puts " "
             puts "Thanks for stopping by!"
+            puts " "
+
         else
             puts " "
             puts "Oops! Please try again!"
             puts " "
         end      
     end
-
-  # def list_films(index_of_titles)
-        #     puts " "
-        #     puts "#{Film.all[index_of_titles].title}"
-        #     puts " "
-        #     puts "#{Film.all[index_of_titles].release}"
-        #     puts " "
-        #     puts "#{Film.all[index_of_titles].image}"
-        #    end
-        
-
-
-        # #this method will input a proper input between 1-10
-        # if input.to_i > 0 && input.to_i <= 10
-        #     index_of_titles = input.to_i - 1
-        # else
-        #     puts "Please try again!"
-        # end 
-        # #has to connect index with object
-        #     object = Film.all[5] 
-        #     moviecard.list_movie_titles[index_of_titles - 1] 
-        # end
-        # #connect movie title with date and image attr
-        # # Film.find_and_show_film_obj
-        # # puts ' '
-        # # #puts
 end
+
+
 
 
 
